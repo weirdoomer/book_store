c4685daa1786ef69b6cc46301fb04e33d8731ffe
weirdoomer <rebelbro95@ya.ru>
2024-05-23 02:31:59 +0300

    view for index.html created

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Product, ProductCategory
+
+
+class IndexView(ListView):
+    model = Product
+    template_name = "products/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["title"] = "Book Store"
+        return context
