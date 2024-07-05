551ddf3f4c675e8c0473d15d3bf4e43a03ce2a6e
weirdoomer <rebelbro95@ya.ru>
2024-07-05 12:25:20 +0300

    updated the Title Mixin to display a dynamic page title

--- common/utils/views/views_mixins.py
+++ common/utils/views/views_mixins.py
@@ -1,7 +1,15 @@
 class TitleMixin:
+    # Переменная класса, которую можно использовать для простых 
+    # (не динамически формируемых) заголовков
     title = None
+
+    # Если нужен не захардкоженный динамически формируемый заголовок,
+    # во вьюхе переопределить метод get_title и вместо self.title
+    # подставить необходимые данные
+    def get_title(self):
+        return self.title
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = self.title
+        context["title"] = self.get_title()
         return context
