6131d9caed704daa85f24df91b0041269246188f
weirdoomer <rebelbro95@ya.ru>
2024-07-05 12:26:42 +0300

    - added a view method ProductDetailView for the product page - added a route for the product page view method ProductDetailView - added a template for the product page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from .views import ProductsListView
+from .views import ProductDetailView, ProductsListView
 
 app_name = "products"
 
@@ -11,4 +11,5 @@ urlpatterns = [
         ProductsListView.as_view(),
         name="category",
     ),
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from common.utils.views.views_mixins import TitleMixin
@@ -34,3 +35,15 @@ class ProductsListView(TitleMixin, ListView):
         context = super().get_context_data()
         context["categories"] = ProductCategory.objects.all()
         return context
+
+
+class ProductDetailView(TitleMixin, DetailView):
+    model = Product
+    template_name = "products/product.html"
+    slug_field = "slug"
+
+    def get_title(self):
+        authors_str = ", ".join(
+            [f"{i.first_name} {i.last_name}" for i in self.object.author.all()]
+        )
+        return f"{self.object.name} ({authors_str})"
