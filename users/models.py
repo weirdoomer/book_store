8eee1c23d75193197e00ae4537f48bbbf31dec0d
weirdoomer <rebelbro95@ya.ru>
2024-07-31 05:55:27 +0300

    User model changed: image field removed

--- users/models.py
+++ users/models.py
@@ -3,5 +3,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to="users_images", null=True, blank=True)
     is_verified_email = models.BooleanField(default=False)
